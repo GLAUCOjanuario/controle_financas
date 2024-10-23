486a309d7cd10f0d092e539940d2eb7e597e9424
GLAUCOjanuário <glaucomanutencao@gmail.com>
2024-10-23 16:38:37 -0300

    first commit

---
+++ myFinancesSystem/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import Header from './components/Header/Header';
+
+import './App.css'
+
+
+function App() {
+
+  return(
+   
+
+    <div>
+     <Header />
+     <Header />
+    </div>
+
+
+  );
+     
+}
+
+export default App
